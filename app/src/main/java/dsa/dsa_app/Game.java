6f9ecb0c4b576e811a8bfe4f8c3e7f715b8c40e8
garretaserra <garretaserra@gmail.com>
2018-05-13 22:54:48 +0200

    Arreglat el manifest per a que el projecte compili correctamen a la versio target del sdk.

--- app/src/main/java/dsa/dsa_app/Game.java
+++ app/src/main/java/dsa/dsa_app/Game.java
@@ -1,26 +1,24 @@
 package dsa.dsa_app;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class LogIn extends AppCompatActivity {
+public class Game extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_game);
         //Remove title bar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         //Remove notification bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
-    public void nextActivity(View view){
-        Intent i = new Intent(this, Game.class);
-        startActivity(i);
-    }
 }
