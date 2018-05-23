a0c7e0019d403b39a76ebe07b2ab69d1c1f8e31d
garretaserra <garretaserra@gmail.com>
2018-05-23 16:15:37 +0200

    Editat el manifest per a que les activities estiguin en horitzontal (landscape) i tambe que estiguin en pantalla completa

--- app/src/main/java/dsa/dsa_app/UserMain.java
+++ app/src/main/java/dsa/dsa_app/UserMain.java
@@ -1,25 +1,17 @@
 package dsa.dsa_app;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class LogIn extends AppCompatActivity {
+public class UserMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_log_in);
-
-    }
-
-    public void nextActivity(View view){
-        Intent i = new Intent(this, UserMain.class);
-        startActivity(i);
+        setContentView(R.layout.activity_user_main);
     }
 }
