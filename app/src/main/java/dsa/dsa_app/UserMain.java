af3a2f0bed039c5aaee268a7028c357099bed7c7
garretaserra <garretaserra@gmail.com>
2018-05-23 17:48:55 +0200

    El botons de la activity ara porten a la activity corresponents

--- app/src/main/java/dsa/dsa_app/UserMain.java
+++ app/src/main/java/dsa/dsa_app/UserMain.java
@@ -1,7 +1,9 @@
 package dsa.dsa_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -13,5 +15,20 @@ public class UserMain extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_main);
+    }
+
+    public void playGame(View view){
+        Intent i = new Intent(this, Game.class);
+        startActivity(i);
+    }
+
+    public void mapList(View view){
+        Intent i = new Intent(this, MapList.class);
+        startActivity(i);
+    }
+
+    public void playerInfo(View view){
+        Intent i = new Intent(this,UserInfo.class);
+        startActivity(i);
     }
 }
