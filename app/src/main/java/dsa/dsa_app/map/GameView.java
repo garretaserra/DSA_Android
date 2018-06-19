a1e55cb11efeacf3113f6c36ffd03278a08e91eb
garretaserra <garretaserra@gmail.com>
2018-06-19 23:00:10 +0200

    Añadido el personaje banquero

--- app/src/main/java/dsa/dsa_app/map/GameView.java
+++ app/src/main/java/dsa/dsa_app/map/GameView.java
@@ -527,6 +527,7 @@ public class GameView extends SurfaceView {
             }
             case "orfanato":{
                 map = orfanato;
+
                 break;
             }
             case "banco":{
---
+++ app/src/main/java/dsa/dsa_app/visuals/Banquero.java
@@ -0,0 +1,63 @@
+package dsa.dsa_app.visuals;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import dsa.dsa_app.App;
+import dsa.dsa_app.R;
+import dsa.dsa_app.map.GameView;
+
+public class Banquero extends Sprite{
+
+    private static Bitmap bmp = BitmapFactory.decodeResource(App.getContext().getResources(), R.drawable.banquero);
+
+    private GameView gameView;
+    private int filas;
+    private int columnas;
+    private int posX;
+    private int posY;
+
+    public Banquero (GameView gameView, int filas, int columnas){
+        this.gameView = gameView;
+        this.filas = filas;
+        this.columnas = columnas;
+    }
+
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+    @Override
+    public int getPosx() {
+        return posX;
+    }
+    @Override
+    public int getPosy() {
+        return posY;
+    }
+    @Override
+    public void draw(Canvas canvas) {
+        posX = columnas*gameView.getWidth()/GameView.orfanato.getAnchura();
+        posY = filas*gameView.getHeight()/GameView.orfanato.getAltura();
+        int width = bmp.getWidth()/3;
+        int height = bmp.getHeight()/4;
+        Rect origen = new Rect(width*1,height*0,width*2,height*1);
+        Rect dest = new Rect(posX, posY, posX+width, posY+height);
+        canvas.drawBitmap(bmp, origen,dest,null);
+    }
+    @Override
+    public void onTouch(GameView gameView, MotionEvent event) {
+        if(gameView.getCharacter().isClose((int)event.getX(),(int)event.getY())){
+            Toast.makeText(App.getContext(), "Soy el banquero", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
