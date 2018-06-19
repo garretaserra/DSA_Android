5165fa4825c45a70e3180a1e3d1d101af8e6099e
Dani <dani.dalfo.f@gmail.com>
2018-06-19 14:49:02 +0200

    splash screen

---
+++ app/src/main/java/dsa/dsa_app/Splash_Screen.java
@@ -0,0 +1,25 @@
+package dsa.dsa_app;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(Splash_Screen.this, LogIn.class);
+                startActivity(intent);
+
+            }
+        },4000);
+    }
+}
